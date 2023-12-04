1f26a8320fb6ed71921196143643abd24862787c
Joel-12 <46884055+Joel-12@users.noreply.github.com>
2023-12-04 15:26:57 +0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,5 +1,5 @@
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+curl -L --output cloudflared.rpm https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-x86_64.rpm && 
 
-sudo dpkg -i cloudflared.deb && 
+sudo yum localinstall -y cloudflared.rpm && 
 
-sudo cloudflared service install eyJhIjoiY2VlMzI4M2Q4ZDU1YzFiYmRmZWIwZmRhYWEwNDVjNDgiLCJ0IjoiMzNjMTc3MDctOWI3NC00NWM2LTkyNzQtODY0M2U4MGJiODMwIiwicyI6Ik5tUm1aVEpsTXpNdFpEWXdaUzAwTURjM0xUazNaREF0TldFMlpETmtPVGM1WVdSayJ9
+sudo cloudflared service install eyJhIjoiY2VlMzI4M2Q4ZDU1YzFiYmRmZWIwZmRhYWEwNDVjNDgiLCJ0IjoiODNkNjQ0ZDMtMWI0ZC00ZmZiLTgzMGItYTlkOGQwYWZiMjgwIiwicyI6IllUaGpNalExWXpndE5XVXhNQzAwTWpnNExUZzRPRGd0WVRnM1lXRTRNV0U0TkRjeCJ9
