56c6a60944224c75e2c6580d9570401502e52821
Joel-12 <46884055+Joel-12@users.noreply.github.com>
2024-01-30 18:05:53 +0300

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -5,3 +5,6 @@ sudo dpkg -i cloudflared.deb &&
 sudo cloudflared service install eyJhIjoiY2VlMzI4M2Q4ZDU1YzFiYmRmZWIwZmRhYWEwNDVjNDgiLCJ0IjoiNDhmNGU2NjMtY2Q0Yy00ZDg3LTg4M2QtZWFlYTllYzdlZGEzIiwicyI6Ik5XWTNNMkUzTlRndE5EY3pPQzAwWTJabUxUZzNPVFV0Wm1NM01EYzVOV0ZrWmpReSJ9
 
 https://www.campcodes.com/projects/php/laravel-ecommerce-system-in-php-mysql/
+
+
+https://drive.google.com/file/d/1q-wMJ750EedCGvxNRyjCsYR4rhIXjYfn/view
